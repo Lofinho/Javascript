c20ddcf01cc63be58d0003203caef4ef99e6789e
Leonardo <leonardo.watson.26@gmail.com>
2022-12-06 01:03:34 -0300

    Javascript Laços de Repetição

--- Exercicio-Web/Site2/EstruturaDeRepeticao/ex02.js
+++ Exercicio-Web/Site2/EstruturaDeRepeticao/ex02.js
@@ -1,2 +1,12 @@
 // Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
 
+let usuario, senha
+
+while(usuario == senha){
+  usuario = prompt('Digite seu Login')
+  senha = prompt('Digite a sua senha')
+  
+  if(usuario == senha){
+    alert('ERRO, a senha não pode ser igual ao usuário');
+  }
+}

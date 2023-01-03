12a174323402cfe8f712af34236598bde936de63
Lofinho <leonardo.watson.26@gmail.com>
2023-01-03 17:45:12 -0300

    Javascript Laços de Repetição

--- Exercicio-Web/Site2/EstruturaDeRepeticao/ex04.js
+++ Exercicio-Web/Site2/EstruturaDeRepeticao/ex04.js
@@ -1,17 +1,19 @@
+// Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
 let capA = 80000;
 let capB = 200000;
 let taxa_paisA = 0.03;
 let taxa_paisB = 0.015;
-let paisA, paisB, newCapA, newCapB
+let paisA, paisB, newCapA, newCapB;
 
-paisA = capA
-paisB = capB
+paisA = capA;
+paisB = capB;
 
-for(let ano = 0; paisA <= paisB; ano++){
-    capA = capA + capA * taxa_paisA
-    capB = capB + capB * taxa_paisB
+for (let ano = 0; paisA <= paisB; ano++) {
+    capA = capA + capA * taxa_paisA;
+    capB = capB + capB * taxa_paisB;
 
-    paisA = capA
-    paisB = capB
-    console.log(ano)
+    paisA = capA;
+    paisB = capB;
+    console.log(ano);
 }
